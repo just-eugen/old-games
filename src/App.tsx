ae8176eb1a6f4d5354def73f56ecc169d1bde997
just-eugene <79237933+just-eugen@users.noreply.github.com>
2025-12-03 01:40:41 +0700

    Added button (#9)
    * Rewind time
    
    * Added button
    
    * lil fix
    
    * lil fix 2
    
    * fix: changed types of export routes, defined anonimus function
    
    * fix: moved handleClick inside the App
    
    ---------
    
    Co-authored-by: just-eugen <eugene@bullshitgaming.localdomain>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './components/button';
+import { Button } from './components/button';
 
 function App() {
+  const handleClick = () => {
+    alert('test');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button text="test" onClick={handleClick} />
     </div>
   );
 }
